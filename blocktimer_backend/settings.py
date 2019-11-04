9c343c38b0926e0d7753ec3fa58f4c30f9702139
Michael Bauer <michael@m-bauer.org>
2019-11-04 12:18:13 +0100

    Allow public access by setting CORS_ORIGIN_ALLOW_ALL

--- blocktimer_backend/settings.py
+++ blocktimer_backend/settings.py
@@ -59,14 +59,8 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'https://localhost:3000',
-    'http://127.0.0.1:3000',
-    'https://127.0.0.1:3000',
-    ]
-
-CORS_ALLOW_CREDENTIALS = True
+# Make API public available
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'blocktimer_backend.urls'
 
